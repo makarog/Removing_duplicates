7072452829e6d5acf028da99c713c5105fc8c12f
Alex Makarov <mackarowsa@gmail.com>
2024-01-07 19:18:00 +0300

    done tests

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def main():
+    def remove_duplicates(array):
+        unique_elements = []
+        underscore_count = 0
+
+        for num in array:
+            if num not in unique_elements:
+                unique_elements.append(num)
+            else:
+                underscore_count += 1
+
+        result = unique_elements + ['_'] * underscore_count
+        return result
+
+    n = int(sys.stdin.readline().rstrip())
+    input_data = sys.stdin.readline().rstrip()
+
+    sorted_array = list(map(int, input_data.split()))
+
+    result = remove_duplicates(sorted_array)
+    output = ' '.join(map(str, result))
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
